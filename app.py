40575cd0fdd2f5ad09a9fb481cd7d1ae701d2723
unknown <ajithrkv981@gmail.com>
2021-09-21 16:09:43 +0200

    make it better

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
+
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])  # route to display the home page
